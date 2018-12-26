4f0b9fcc1f8ceb370511e47b1ae1662ca53fefc0
zhangwenhao <hizhangwenhao@gmail.com>
2018-12-26 16:02:07 +0900

    update

--- appa/src/main/java/com/example/itarchitecture/aidl/RemindService.java
+++ appa/src/main/java/com/example/itarchitecture/aidl/RemindService.java
@@ -4,8 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.os.RemoteException;
-
 import java.util.ArrayList;
 import java.util.List;
 
